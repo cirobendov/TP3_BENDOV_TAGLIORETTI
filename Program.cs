a0994656acf662d9ebc3202452502dd09d0f4359
CIRO BEN DOV <48111117@ort>
2024-05-10 15:57:32 -0300

    siguiendo

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 
+Dictionary <int, double> Entradas = new Dictionary<int, double>();
+    Entradas.Add(1, 45000);
+    Entradas.Add(2, 60000);
+    Entradas.Add(3, 30000);
+    Entradas.Add(4, 100000);
 
 static void NuevaInscripcion()
 {
     string apellido, nombre; 
     DateTime fechaInscripcion = DateTime.Now; 
     int tipoentrada, cant, dni;
+    
 
     Console.WriteLine("Ingrese el nombre");
     nombre = Console.ReadLine();
     Console.WriteLine("Ingrese el apellido");
     apellido = Console.ReadLine();
-    Console.WriteLine("Ingrese el tipo de entrada");
-    tipoentrada = int.Parse(Console.ReadLine());
+    do
+    {
+        Console.WriteLine("Ingrese el tipo de entrada");
+        tipoentrada = int.Parse(Console.ReadLine());
+    } while(tipoentrada < 1 || tipoentrada > 4);
     Console.WriteLine("Ingrese la cantidad");
     cant = int.Parse(Console.ReadLine());
     Console.WriteLine("Ingrese su dni");
--- Ticketera.cs
+++ Ticketera.cs
@@ -16,13 +16,36 @@ static class Ticketera
         return UltimoIDEntrada;
            
     }
-    public static void BuscarCliente(int ID)
+    public static Clientes BuscarCliente(int ID, Dictionary <int, Clientes> dicCLientes)
     {
+        bool existe;
+        Clientes clienteObtenido = null;
+        existe =  dicCLientes.ContainsKey(ID);
+        if (existe == true)
+        {
+            clienteObtenido = dicCLientes[ID];
+        }
+        else 
+        {
+            Console.WriteLine("No se ha encontrado un cliente con ese ID");
+        }
+        return clienteObtenido;
 
     }
-    public static int CambiarEntrada(int ID, int tipo, int cantidad)
+    public static bool CambiarEntrada(int ID, int tipo, int cantidad, Dictionary<int, double> Entradas, Dictionary <int, Clientes> dicCLientes)
     {
-
+        int IDIngresado;
+        Console.WriteLine("Ingrese su id");
+        IDIngresado = int.Parse(Console.ReadLine());
+        if (BuscarCliente(IDIngresado, dicCLientes) != null)
+        {
+            do
+            {
+                Console.WriteLine("Ingrese el nuevo tipo de entrada");
+                tipo = int.Parse(Console.ReadLine());
+            } while(Entradas.ContainsKey(tipo));
+            if(Entradas.Values > dicCLientes[ID].)
+        }
     }
     public static List<string> EstadisticasTicketera()
     {
